93b38ceff4efe8d2d193c6d3cc167f875f65fb88
Paul Chen <paul.chen@thinkpower-info.com>
2019-12-24 16:10:23 +0800

    update

--- vue-example/src/views/Tutorial.vue
+++ vue-example/src/views/Tutorial.vue
@@ -5,44 +5,93 @@
     <!-- Day 1 - Start -->
     <h2>Day 1(內容會視成員對網頁技術的了解程度做調整)</h2>
 
-    <p>課程介紹</p>
+    <h3>課程介紹</h3>
+    <p>介紹課程內容</p>
+
+    <h3>自我介紹</h3>
     <p>全員自我介紹</p>
-    <p>
-      環境建置：VSCODE、git、github
-      帳號申請、sourcetree、ssh(mac、windows)、NODE.js 安裝
-    </p>
-    <p>
+
+    <h3>環境建置</h3>
+    <ul>
+      <li>VSCODE - 安裝、基本設定與套件安裝</li>
+      <!-- <p>
       VSCODE套件：Code Spell Checker、Vetur、ESLint、Prettier、Chinese Language
-    </p>
-    <p>Html： Tag 介紹與使用</p>
-    <p>CSS：選擇器、排版、基本樣式、Bootstrap</p>
-    <p>Debut：F12 查看 element</p>
+    </p> -->
+      <li>Git</li>
+      <li>Github - 帳號申請</li>
+      <li>Sourcetree - 版控 GUI 程式 Sourcetree 安裝及設定</li>
+      <li>SSH</li>
+      <li>NODE.js</li>
+    </ul>
+
+    <h3>HTML</h3>
+    <ul>
+      <li>
+        常用 html element 介紹 - h1 - 6, div, p, input ...，基本用法與介紹
+      </li>
+      <li>
+        html element 講解 - 講解 html element 的結構(tag, content, attributes)
+      </li>
+    </ul>
+
+    <h3>CSS</h3>
+    <ul>
+      <li>基礎 - 語法、選擇器、引入方式比較</li>
+      <li>基本排版 - html 及 css 的排版練習</li>
+      <li>開發者工具 - 在瀏覽器中使用開發者工具(F12) 查看及編輯 element</li>
+      <li>Bootstrap</li>
+    </ul>
     <!-- Day 1 - End -->
 
     <!-- Day 2 - Start -->
     <h2>Day 2</h2>
-    <p>JS 基礎：變數(variable)、方法(function)、物件(object)、陣列(array)</p>
-    <p>代碼風格：命名規範、排版</p>
-    <p>Debug：console</p>
-    <p>Vue 框架介紹</p>
-    <p>安裝 chrome Vue.js devtools</p>
-    <p>
-      Vue 基礎：new Vue、el(選擇器)、data、computed、v-bind、v-on、v-if、v-for
-    </p>
+
+    <h3>Javascript</h3>
+    <ul>
+      <li>
+        基礎 - 變數(variable)、方法(function)、物件(object)、陣列(array)
+      </li>
+      <li>註解</li>
+      <li>代碼風格 - 命名規範、排版</li>
+      <li>Debug - Console</li>
+    </ul>
+
+    <h3>Vue</h3>
+    <ul>
+      <li>介紹</li>
+      <li>安裝 chrome Vue.js devtools</li>
+      <li>
+        基礎教學 - 以 CDN 的方式引入
+        Vue，以清單搜尋範例講解vue的基本觀念及語法，new
+        Vue、el(選擇器)、data、computed、v-bind、v-on、v-if、v-for
+      </li>
+    </ul>
     <!-- Day 2 - End -->
 
     <!-- Day 3 - Start -->
     <h2>Day 3</h2>
-    <p>生命週期</p>
-    <p>Vue template</p>
-    <p>Vue Cli：解說 spa 及如何使用 vue 建立 spa 網站</p>
-    <p>Router：由前端控制網站 router</p>
-    <p>ES Lint：代碼風格規範工具</p>
-    <p>npm：安裝 package</p>
-    <p>
+
+    <h3>Vue Cli</h3>
+    <ul>
+      <li>介紹 - 解說 spa 及如何使用 vue-cli 建立 spa 網站，專案架構說明</li>
+      <li>npm：安裝 package</li>
+      <li>ES Lint、Prettier：代碼風格規範及排版工具介紹</li>
+      <li>vue-router - 由前端控制網站 router</li>
+      <li>Vue template - 講解.vue檔及如何編寫</li>
+      <li>生命週期 - 介紹 vue component 的生命週期</li>
+    </ul>
+
+    <h3>Restful api</h3>
+    <ul>
+      <li>介紹</li>
+      <li>模擬</li>
+      <li>postman</li>
+      <li>axios</li>
+    </ul>
+    <!-- <p>
       API：Restful Api 解說、如果沒有提供API則使用 json-server 模擬，先使用
       postman 熟悉 api 的操作，再使用 axios 練習
-    </p>
+    </p> -->
     <!-- Day 3 - End -->
 
     <!-- Day 4 - Start -->
